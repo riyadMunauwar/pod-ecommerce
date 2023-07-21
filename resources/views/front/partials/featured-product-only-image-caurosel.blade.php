a6f74add4a1b551aa7457f1088e5fd43bee26e48
riyad <hifazakhatun1108@gmail.com>
2023-07-21 15:50:20 +0600

    image slider added

--- resources/views/front/partials/featured-product-only-image-caurosel.blade.php
+++ resources/views/front/partials/featured-product-only-image-caurosel.blade.php
@@ -1,160 +1,26 @@
+@php
+
+    $products = \App\Models\Product::inRandomOrder()->take(20)->get();
+
+@endphp
+
 <!-- Insta Post Start -->
 <div class="container-fluid relative">
     <div class="grid grid-cols-1 relative">
+
         <div class="tiny-twelve-item">
-            <div class="tiny-slide">
-                <div class="card border-0 rounded-0">
-                    <div class="card-body p-0">
-                        <a href="assets/images/cafe/coffee-cup/01.jpg" class="lightbox d-inline-block" title="">
-                            <img src="assets/images/cafe/coffee-cup/01.jpg" class="" alt="Insta Post">
-                            <div class="overlay bg-dark"></div>
-                        </a>
+            @foreach($products as $product)
+                <div class="tiny-slide">
+                    <div class="card border-0 rounded-0">
+                        <div class="card-body p-0">
+                            <a href="{{ $product->thumbnailUrl('small') }}" class="lightbox d-inline-block" title="">
+                                <img src="{{ $product->thumbnailUrl('small') }}" class="" alt="Insta Post">
+                                <div class="overlay bg-dark"></div>
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            
-            <div class="tiny-slide">
-                <div class="card border-0 rounded-0">
-                    <div class="card-body p-0">
-                        <a href="assets/images/cafe/coffee-cup/02.jpg" class="lightbox d-inline-block" title="">
-                            <img src="assets/images/cafe/coffee-cup/02.jpg" class="" alt="Insta Post">
-                            <div class="overlay bg-dark"></div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="tiny-slide">
-                <div class="card border-0 rounded-0">
-                    <div class="card-body p-0">
-                        <a href="assets/images/cafe/coffee-cup/03.jpg" class="lightbox d-inline-block" title="">
-                            <img src="assets/images/cafe/coffee-cup/03.jpg" class="" alt="Insta Post">
-                            <div class="overlay bg-dark"></div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="tiny-slide">
-                <div class="card border-0 rounded-0">
-                    <div class="card-body p-0">
-                        <a href="assets/images/cafe/coffee-cup/04.jpg" class="lightbox d-inline-block" title="">
-                            <img src="assets/images/cafe/coffee-cup/04.jpg" class="" alt="Insta Post">
-                            <div class="overlay bg-dark"></div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="tiny-slide">
-                <div class="card border-0 rounded-0">
-                    <div class="card-body p-0">
-                        <a href="assets/images/cafe/coffee-cup/05.jpg" class="lightbox d-inline-block" title="">
-                            <img src="assets/images/cafe/coffee-cup/05.jpg" class="" alt="Insta Post">
-                            <div class="overlay bg-dark"></div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="tiny-slide">
-                <div class="card border-0 rounded-0">
-                    <div class="card-body p-0">
-                        <a href="assets/images/cafe/coffee-cup/06.jpg" class="lightbox d-inline-block" title="">
-                            <img src="assets/images/cafe/coffee-cup/06.jpg" class="" alt="Insta Post">
-                            <div class="overlay bg-dark"></div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="tiny-slide">
-                <div class="card border-0 rounded-0">
-                    <div class="card-body p-0">
-                        <a href="assets/images/cafe/coffee-cup/01.jpg" class="lightbox d-inline-block" title="">
-                            <img src="assets/images/cafe/coffee-cup/01.jpg" class="" alt="Insta Post">
-                            <div class="overlay bg-dark"></div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="tiny-slide">
-                <div class="card border-0 rounded-0">
-                    <div class="card-body p-0">
-                        <a href="assets/images/cafe/coffee-cup/02.jpg" class="lightbox d-inline-block" title="">
-                            <img src="assets/images/cafe/coffee-cup/02.jpg" class="" alt="Insta Post">
-                            <div class="overlay bg-dark"></div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="tiny-slide">
-                <div class="card border-0 rounded-0">
-                    <div class="card-body p-0">
-                        <a href="assets/images/cafe/coffee-cup/03.jpg" class="lightbox d-inline-block" title="">
-                            <img src="assets/images/cafe/coffee-cup/03.jpg" class="" alt="Insta Post">
-                            <div class="overlay bg-dark"></div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="tiny-slide">
-                <div class="card border-0 rounded-0">
-                    <div class="card-body p-0">
-                        <a href="assets/images/cafe/coffee-cup/04.jpg" class="lightbox d-inline-block" title="">
-                            <img src="assets/images/cafe/coffee-cup/04.jpg" class="" alt="Insta Post">
-                            <div class="overlay bg-dark"></div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="tiny-slide">
-                <div class="card border-0 rounded-0">
-                    <div class="card-body p-0">
-                        <a href="{{ asset('techwind-assets/images/cafe/coffee-cup/05.jpg') }}" class="lightbox d-inline-block" title="">
-                            <img src="{{ asset('techwind-assets/images/cafe/coffee-cup/05.jpg') }}" class="" alt="Insta Post">
-                            <div class="overlay bg-dark"></div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="tiny-slide">
-                <div class="card border-0 rounded-0">
-                    <div class="card-body p-0">
-                        <a href="assets/images/cafe/coffee-cup/06.jpg" class="lightbox d-inline-block" title="">
-                            <img src="assets/images/cafe/coffee-cup/06.jpg" class="" alt="Insta Post">
-                            <div class="overlay bg-dark"></div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="tiny-slide">
-                <div class="card border-0 rounded-0">
-                    <div class="card-body p-0">
-                        <a href="assets/images/cafe/coffee-cup/05.jpg" class="lightbox d-inline-block" title="">
-                            <img src="assets/images/cafe/coffee-cup/05.jpg" class="" alt="Insta Post">
-                            <div class="overlay bg-dark"></div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="tiny-slide">
-                <div class="card border-0 rounded-0">
-                    <div class="card-body p-0">
-                        <a href="assets/images/cafe/coffee-cup/06.jpg" class="lightbox d-inline-block" title="">
-                            <img src="{{ asset('techwind-assets/images/cafe/coffee-cup/06.jpg') }}" class="" alt="Insta Post">
-                            <div class="overlay bg-dark"></div>
-                        </a>
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </div>
 
         <div class="absolute top-2/4 -translate-y-2/4 start-2/4 ltr:-translate-x-2/4 rtl:translate-x-2/4 text-center hidden md:block">
