05577edcab6a312326f8b610c63a6b94fbe2217e
riyad <hifazakhatun1108@gmail.com>
2023-07-21 19:43:35 +0600

    favicon added

--- resources/views/designer/layouts/designer-layout.blade.php
+++ resources/views/designer/layouts/designer-layout.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" type="image/x-icon">
+
         <title>{{ $title }}</title>
 
         <!-- Fonts -->
