ffc2ab3ebd44101fbdba082330644ea0ed7e78e4
riyad <hifazakhatun1108@gmail.com>
2023-07-21 20:27:59 +0600

    recent/popular products added

--- app/Http/Livewire/Front/GeneralArchieve.php
+++ app/Http/Livewire/Front/GeneralArchieve.php
@@ -3,11 +3,31 @@
 namespace App\Http\Livewire\Front;
 
 use Livewire\Component;
+use App\Models\Product;
 
 class GeneralArchieve extends Component
 {
+
+    public $products;
+
+
+    public function mount()
+    {
+        $this->products = $this->getProducts();
+    }
+
     public function render()
     {
         return view('front.components.general-archieve');
+    }
+
+    private function getProducts()
+    {
+        return Product::inRandomOrder()->take(12)->get();
+    }
+
+    private function getCategory()
+    {
+        return Category::where('slug', request()->category_slug)->first();
     }
 }
