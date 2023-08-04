ce7ad36c0d578fdaaa11a58933276b7ce776db88
riyad <hifazakhatun1108@gmail.com>
2023-08-04 18:50:45 +0600

    deploy

--- app/Http/Livewire/Admin/EditVariation.php
+++ app/Http/Livewire/Admin/EditVariation.php
@@ -14,8 +14,6 @@ class EditVariation extends Component
     use WithSweetAlert;
 
     public $is_edit_mode_on = false;
-    public $new_image;
-    public $old_image;
 
     public $variation;
 
@@ -25,17 +23,7 @@ class EditVariation extends Component
 
 
     protected $rules = [
-        'variation.sale_price' => ['required', 'numeric'],
-        'variation.regular_price' => ['required', 'numeric'],
         'variation.stock_qty' => ['required', 'integer'],
-        'variation.weight' => ['nullable', 'numeric'],
-        'variation.sku' => ['nullable', 'string'],
-        'variation.height' => ['nullable', 'numeric'],
-        'variation.width' => ['nullable', 'numeric'],
-        'variation.length' => ['nullable', 'numeric'],
-        'variation.is_published' => ['required', 'boolean'],
-        'old_image' => ['required_without_all:new_image', 'string'],
-        'new_image' => ['required_without_all:old_image'],
     ];
 
 
@@ -47,45 +35,10 @@ class EditVariation extends Component
 
     public function updated($attribute, $value)
     {
-
-        switch($attribute){
-
-            case 'variation.sale_price': 
-                if(empty($value)){
-                    $this->variation->sale_price = null;
-                }
-                break;
-            case 'variation.regular_price':
-                if(empty($value)){
-                    $this->variation->regular_price = null;
-                }
-                break;
-            case 'variation.stock_qty':
-                if(empty($value)){
-                    $this->variation->stock_qty = null;
-                }
-                break;
-            case 'variation.width':
-                if(empty($value)){
-                    $this->variation->width = null;
-                }
-                break;
-            case 'variation.height':
-                if(empty($value)){
-                    $this->variation->height = null;
-                }
-                break;
-            case 'variation.length':
-                if(empty($value)){
-                    $this->variation->length = null;
-                }
-                break;
-            case 'variation.weight':
-                if(empty($value)){
-                    $this->variation->weight = null;
-                }
-                break;
- 
+        if($attribute === 'variation.stock_qty'){
+            if(empty($value)){
+                $this->variation->stock_qty = null;
+            }
         }
     }
 
@@ -93,11 +46,6 @@ class EditVariation extends Component
     {
 
         $this->validate();
-
-        if($this->new_image){
-            $this->variation->addMedia($this->new_image)->toMediaCollection('image');
-        }
-
 
         if($this->variation->save()){
 
@@ -122,17 +70,8 @@ class EditVariation extends Component
 
         $this->variation = Variation::find($id);
 
-        $this->old_image = $this->variation->imageUrl('small');
-
         $this->is_edit_mode_on = true;
 
-    }
-
-
-    public function removeImage()
-    {
-        $this->new_image->delete();
-        $this->new_image = null;
     }
 
 
--- resources/views/admin/components/edit-variation.blade.php
+++ resources/views/admin/components/edit-variation.blade.php
@@ -16,79 +16,10 @@
             <div class="grid grid-cols-1 md:grid-cols-2 gap-5">
 
                 <div>
-                    <x-label  for="" value="{{ __('Sale Price') }}" />
-                    <x-input wire:model.debounce="variation.sale_price" id="" class="block mt-1 h-8 w-full" type="number" />
-                </div>
-
-                <div>
-                    <x-label  for="reg_price" value="{{ __('Regular Price') }}" />
-                    <x-input wire:model.debounce="variation.regular_price" id="reg_price" class="block mt-1 h-8 w-full" type="number" />
-                </div>
-
-                <div>
                     <x-label  for="stock" value="{{ __('Stock') }}" />
                     <x-input wire:model.debounce="variation.stock_qty" id="stock" class="block mt-1 h-8 w-full" type="number" />
                 </div>
 
-                <div>
-                    <x-label  for="sku" value="{{ __('Sku') }}" />
-                    <x-input wire:model.debounce="variation.sku" id="sku" class="block mt-1 h-8 w-full" type="text" />
-                </div>
-
-                <div>
-                    <x-label  for="weight" value="{{ __('Weight') }}" />
-                    <x-input wire:model.debounce="variation.weight" id="sku" class="block mt-1 h-8 w-full" type="number" />
-                </div>
-
-                <div>
-                    <x-label  for="height" value="{{ __('Height') }}" />
-                    <x-input wire:model.debounce="variation.height" id="height" class="block mt-1 h-8 w-full" type="number" />
-                </div>
-
-                <div>
-                    <x-label  for="Width" value="{{ __('Width') }}" />
-                    <x-input wire:model.debounce="variation.width" id="width" class="block mt-1 h-8 w-full" type="number" />
-                </div>
-
-                <div>
-                    <x-label  for="length" value="{{ __('Length') }}" />
-                    <x-input wire:model.debounce="variation.length" id="length" class="block mt-1 h-8 w-full" type="number" />
-                </div>
-
-                <div class="col-span-2">
-                    <x-label for="parent" value="{{ __('Image') }}" />
-
-                    @if(!$new_image && $old_image)
-                        <div class="flex items-center justify-center mb-3">
-                            <img class="h-20 h-20 object-contain block" src="{{ $old_image ?? '' }}">
-                        </div>
-                    @endif
-
-                    @if($new_image)
-                        <div>
-                            <div class="flex items-center justify-center">
-                                @if ($new_image)
-                                    <img class="w-20 h-20 object-contain block" src="{{ $new_image->temporaryUrl() }}">
-                                @endif
-                            </div>
-                            <div class="flex items-center justify-center mt-2">
-                                <button wire:click.debounce="removeImage" type="button" class="inline-flex items-center px-2 py-1 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest ">Remove</button>
-                            </div>
-                        </div>
-                    @else
-                        <div>
-                            <div class="flex items-center justify-center">
-                                <label class="w-full flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue hover:text-gray-800">
-                                    <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                        <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
-                                    </svg>
-                                    <span class="mt-2 text-base leading-normal">Change Image</span>
-                                    <input wire:model="new_image" type='file' class="hidden" />
-                                </label>
-                            </div>
-                        </div>
-                    @endif
-                </div>
 
                 <div class="block">
                     <label for="is_published" class="flex items-center">
@@ -107,7 +38,6 @@
                     </x-button>
                 </div>
             </div>
-            <x-ui.text-loading-spinner loadingText="Uploading..." wire:loading.flex wire:target="new_image" />
             <x-ui.loading-spinner wire:loading.flex wire:target="cancelEditMode, updateVariation" />
         </div>
     </x-ui.edit-modal>
