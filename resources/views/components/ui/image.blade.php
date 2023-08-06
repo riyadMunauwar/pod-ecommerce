6095a0cefde08ddf53e04df945f194695f922323
riyad <hifazakhatun1108@gmail.com>
2023-08-06 12:56:13 +0600

    deploy

---
+++ resources/views/components/ui/image.blade.php
@@ -0,0 +1,17 @@
+@props(['design' => null, 'src' => ''])
+
+
+<div class="absolute inset-0 w-full h-full">
+
+    <div class="absolute w-full h-full inset-0">
+        <img class="block w-full h-full" scr="{{ $src }}">
+    </div>
+
+    <div style="background-color: rgba(0, 0, 0, .{{ $design->opacity }} )" class="absolute w-full h-full inset-0"></div>
+
+  
+    <div style="top: {{ $design->design_top }}%; left: {{ $design->design_left }}%; width: {{ $design->design_width }}%; height: {{ $design->design_height }}%; transform: rorate({{ $design->design_rotate_angel }}deg);" class="absolute">
+        <img class="block w-full h-full" src="{{ $design->designUrl() }}" alt="">
+    </div>
+   
+</div>
--- resources/views/front/partials/popular-products.blade.php
+++ resources/views/front/partials/popular-products.blade.php
@@ -1,6 +1,6 @@
 @php
 
-    $products = \App\Models\Product::inRandomOrder()->take(4)->get();
+    $designs = \App\Models\Design::inRandomOrder()->take(4)->get();
 
 @endphp
 
@@ -12,10 +12,11 @@
         </div><!--end grid-->
 
         <div class="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 mt-8 gap-[30px]">
-            @foreach($products as $product)
+            @foreach($designs as $design)
                 <div class="group">
-                    <div class="relative overflow-hidden shadow dark:shadow-gray-800 group-hover:shadow-lg group-hover:dark:shadow-gray-800 rounded-md transition-all duration-500">
-                        <img src="{{ $product->thumbnailUrl('small') }}" alt="">
+                    <div class="relative aspect-square overflow-hidden shadow dark:shadow-gray-800 group-hover:shadow-lg group-hover:dark:shadow-gray-800 rounded-md transition-all duration-500">
+                  
+                        <x-ui.image :design="$design" :src="$design->product->thumbnailUrl('small')"
 
                         <div class="absolute -bottom-20 group-hover:bottom-3 start-3 end-3 transition-all duration-500">
                             <a href="{{ route('cart') }}" class="btn bg-slate-900 border-slate-900 text-white w-full rounded-md">Add to Cart</a>
@@ -23,7 +24,7 @@
 
                         <ul class="list-none absolute top-[10px] end-4 opacity-0 group-hover:opacity-100 transition-all duration-500">
                             <li><a href="javascript:void(0)" class="btn btn-icon btn-sm rounded-full bg-indigo-600 hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700 text-white"><i class="mdi mdi-heart"></i></a></li>
-                            <li class="mt-1"><a href="{{ route('product-detail', ['slug' => $product->slug, 'id' => $product->id]) }}" class="btn btn-icon btn-sm rounded-full bg-indigo-600 hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700 text-white"><i class="mdi mdi-eye-outline"></i></a></li>
+                            <li class="mt-1"><a href="" class="btn btn-icon btn-sm rounded-full bg-indigo-600 hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700 text-white"><i class="mdi mdi-eye-outline"></i></a></li>
                             <li class="mt-1"><a href="javascript:void(0)" class="btn btn-icon btn-sm rounded-full bg-indigo-600 hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700 text-white"><i class="mdi mdi-bookmark-outline"></i></a></li>
                         </ul>
 
@@ -33,9 +34,9 @@
                     </div>
 
                     <div class="mt-4">
-                        <a href="{{ route('product-detail', ['slug' => $product->slug, 'id' => $product->id]) }}" class="hover:text-indigo-600 text-lg font-semibold">{{ $product->name ?? '' }}</a>
+                        <a href="{{ route('product-detail', ['slug' => $product->slug, 'id' => $product->id]) }}" class="hover:text-indigo-600 text-lg font-semibold">{{ $design->title ?? '' }}</a>
                         <div class="flex justify-between items-center mt-1">
-                            <p class="text-green-600">{{ $product->sale_price ?? '' }}<del class="text-red-600">{{ $product->regular_price ?? '' }}</del></p>
+                            <p class="text-green-600">{{ $design->design_sale_price ?? '' }}<del class="text-red-600">{{ $design->design_regular_price ?? '' }}</del></p>
                             <ul class="font-medium text-amber-400 list-none">
                                 <li class="inline"><i class="mdi mdi-star"></i></li>
                                 <li class="inline"><i class="mdi mdi-star"></i></li>
