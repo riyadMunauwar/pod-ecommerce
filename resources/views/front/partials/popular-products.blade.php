0a6592b4a8261f261e9a02f7b89e76f09f9e4444
riyad <hifazakhatun1108@gmail.com>
2023-08-06 12:59:58 +0600

    deploy

--- resources/views/front/partials/popular-products.blade.php
+++ resources/views/front/partials/popular-products.blade.php
@@ -16,7 +16,7 @@
                 <div class="group">
                     <div class="relative aspect-square overflow-hidden shadow dark:shadow-gray-800 group-hover:shadow-lg group-hover:dark:shadow-gray-800 rounded-md transition-all duration-500">
                   
-                        <x-ui.image :design="$design" :src="$design->product->thumbnailUrl('small')"
+                        <x-ui.image :design="$design" :src="$design->product->thumbnailUrl('small')" />
 
                         <div class="absolute -bottom-20 group-hover:bottom-3 start-3 end-3 transition-all duration-500">
                             <a href="{{ route('cart') }}" class="btn bg-slate-900 border-slate-900 text-white w-full rounded-md">Add to Cart</a>
@@ -34,7 +34,7 @@
                     </div>
 
                     <div class="mt-4">
-                        <a href="{{ route('product-detail', ['slug' => $product->slug, 'id' => $product->id]) }}" class="hover:text-indigo-600 text-lg font-semibold">{{ $design->title ?? '' }}</a>
+                        <a href="" class="hover:text-indigo-600 text-lg font-semibold">{{ $design->title ?? '' }}</a>
                         <div class="flex justify-between items-center mt-1">
                             <p class="text-green-600">{{ $design->design_sale_price ?? '' }}<del class="text-red-600">{{ $design->design_regular_price ?? '' }}</del></p>
                             <ul class="font-medium text-amber-400 list-none">
