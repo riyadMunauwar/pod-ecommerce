2e38df1f3728edcae070b095b80b7a5b4b372df2
riyad <hifazakhatun1108@gmail.com>
2023-08-06 13:07:01 +0600

    deploy

--- resources/views/components/ui/image.blade.php
+++ resources/views/components/ui/image.blade.php
@@ -4,13 +4,13 @@
 <div class="absolute inset-0 w-full h-full">
 
     <div class="absolute w-full h-full inset-0">
-        <img class="block w-full h-full" scr="{{ $src }}">
+        <img class="block w-full h-full" src="{{ $src }}">
     </div>
 
-    <div style="background-color: rgba(0, 0, 0, .{{ $design->opacity }} )" class="absolute w-full h-full inset-0"></div>
+    <div style="background-color: rgba(0, 0, 0, .{{ $design->desing_opacity }} )" class="absolute w-full h-full inset-0"></div>
 
   
-    <div style="top: {{ $design->design_top }}%; left: {{ $design->design_left }}%; width: {{ $design->design_width }}%; height: {{ $design->design_height }}%; transform: rorate({{ $design->design_rotate_angel }}deg);" class="absolute">
+    <div style="top: {{ $design->design_top }}%; left: {{ $design->design_left }}%; width: {{ $design->design_width }}%; height: {{ $design->design_height }}%; transform: rotate({{ $design->design_rotate_angel }}deg);" class="absolute">
         <img class="block w-full h-full" src="{{ $design->designUrl() }}" alt="">
     </div>
    
