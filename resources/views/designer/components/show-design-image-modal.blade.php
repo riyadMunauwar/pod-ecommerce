d90c54d3199cbb4955dc9dbf879145130673c029
riyad <hifazakhatun1108@gmail.com>
2023-08-09 11:50:42 +0600

    deploy

--- resources/views/designer/components/show-design-image-modal.blade.php
+++ resources/views/designer/components/show-design-image-modal.blade.php
@@ -10,49 +10,6 @@
                         </svg>
                     </span>
                 </div>
-
-                <h1 class="font-bold text-md mb-4">{{ $product_name ?? '' }}</h1>
-                <x-validation-errors class="mb-4" />
-
-                <div class="grid grid-cols-1 md:grid-cols-2 gap-5">
-                    <div class="">
-                        <x-label for="price" value="{{ __('Purchase Price') }}" />
-                        <x-input wire:model.debounce="price" id="price" step=any class="block mt-1 h-8 w-full" type="number" />
-                    </div>
-                    <div class="">
-                        <x-label for="quantity" value="{{ __('Quantity') }}" />
-                        <x-input wire:model.debounce="qty" id="quantity"  class="block mt-1 h-8 w-full" type="number" />
-                    </div>
-                    <div class="">
-                        <x-label for="total" value="{{ __('Total Purchase Price') }}" />
-                        <x-input disabled value="{{$this->total_purchase_price}}" step=any id="total" class="block mt-1 h-8 w-full cursor-not-allowed" type="text" />
-                    </div>
-                    <div class="">
-                        <x-label for="paid_to" value="{{ __('Paid To Supplier') }}" />
-                        <x-input wire:model.debounce="paid_to_supplier" step=any id="paid_to" class="block mt-1 h-8 w-full" type="number" />
-                    </div>
-
-                    <div class="col-span-2">
-                        <x-label for="brand" class="block mb-1" value="{{ __('Supplier') }}" />
-                        <x-ui.select wire:model.debounce="supplier_id" id="brand" class="block h-8 text-sm w-full">
-                            <option value="">None</option>
-                            @foreach($suppliers ?? [] as $supplier)
-                                <option value="{{ $supplier->id }}">{{ $supplier->name ?? '' }}</option>
-                            @endforeach
-                        </x-ui.select>
-                    </div>
-
-                    <div class="col-span-2 flex items-center justify-end">
-                        <x-button wire:click.debounce="addStock" type="button" class="ml-4">
-                            {{ __('Add') }}
-                        </x-button>
-                        <x-button wire:click.debounce="cancelAddStock" type="button" class="ml-4">
-                            {{ __('Cancel') }}
-                        </x-button>
-                    </div>
-
-                </div>
-
             </div>
         </x-ui.edit-modal>
         <x-ui.loading-spinner wire:loading.flex wire:target="addStock, cancelAddStock" />
