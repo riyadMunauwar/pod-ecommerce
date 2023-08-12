4e7c57a04da618277483a2bf95adab1abae6ec7f
riyad <hifazakhatun1108@gmail.com>
2023-08-12 11:14:10 +0600

    deploy

--- resources/views/front/components/partials/most-viewed-product.blade.php
+++ resources/views/front/components/partials/most-viewed-product.blade.php
@@ -8,7 +8,7 @@
             @foreach($products as $product)
                 <div class="group">
                     <div class="relative overflow-hidden shadow dark:shadow-gray-800 group-hover:shadow-lg group-hover:dark:shadow-gray-800 rounded-md transition-all duration-500">
-                        <x-ui.image :design="$product" :src="{{ $product->product->thumbnailUrl('small') }}">
+                        <x-ui.image :design="$product" :src="$product->product->thumbnailUrl('small')">
 
                         <div class="absolute -bottom-20 group-hover:bottom-3 start-3 end-3 transition-all duration-500">
                             <a href="{{ route('cart') }}" class="btn bg-slate-900 border-slate-900 text-white w-full rounded-md">Add to Cart</a>
@@ -22,9 +22,9 @@
                     </div>
 
                     <div class="mt-4">
-                        <a href="{{ route('product-detail', ['slug' => $product->slug, 'id' => $product->id]) }}" class="hover:text-indigo-600 text-lg font-semibold">{{ $product->name ?? '' }}</a>
+                        <a href="" class="hover:text-indigo-600 text-lg font-semibold">{{ $product->name ?? '' }}</a>
                         <div class="flex justify-between items-center mt-1">
-                            <p class="text-green-600">{{ $product->sale_price ?? '' }} <del class="text-red-600">{{ $product->regular_price ?? '' }}</del></p>
+                            <p class="text-green-600">{{ $product->salePrice() ?? '' }} <del class="text-red-600">{{ $product->regular_price ?? '' }}</del></p>
                             <ul class="font-medium text-amber-400 list-none">
                                 <li class="inline"><i class="mdi mdi-star"></i></li>
                                 <li class="inline"><i class="mdi mdi-star"></i></li>
