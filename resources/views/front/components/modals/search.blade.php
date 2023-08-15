4e359a11d0cc4fa9d7f84f08b38d398230b94a84
riyad <hifazakhatun1108@gmail.com>
2023-08-15 11:56:15 +0600

    deploy

--- resources/views/front/components/modals/search.blade.php
+++ resources/views/front/components/modals/search.blade.php
@@ -25,10 +25,10 @@
 
                 <!-- List -->
                 <div class="mt-5 search-scroll-bar overflow-y-scroll h-96">
-                    <div class="flex gap-5">
+                    <div class="grid  gap-4 grid-cols-2 md:grid-cols-3">
                         @foreach($designs as $design)
-                            <div class="w-1/3">
-                                <div class="aspect-square">
+                            <div>
+                                <div class="relative aspect-square">
                                     <x-ui.image :design="$design" :src="$design->product->thumbnailUrl('small')" />
                                 </div>
                                 <div>
